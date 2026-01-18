d71261fec78a5752b8a1cbca2ae21437a62f90d2
HammadAlix <221846@students.au.edu.pk>
2026-01-18 14:30:00 +0500

    add: fxmanifest resource configuration

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game       'gta5'
+lua54      'yes'
+dependency 'qb-core'
+
+shared_scripts {
+    'config.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+
